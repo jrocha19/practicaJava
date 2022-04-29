0cedf272303f53707e658a934f13d4ad5db3586f
jrocha19 <jrocha19@ilg.cat>
2022-04-29 15:23:26 +0200

    Primer

---
+++ src/cat/ilg/Institut.java
@@ -0,0 +1,15 @@
+package cat.ilg;
+
+public class Institut {
+    //Atributos
+    private String nom;
+    //Metodos
+    public void afegirProfe (String profe){
+        this.profe = profe;
+    }
+
+    public void afegirEstudiant (String estudiant){
+        this.estudiant = estudiant;
+    }
+    public void imprimirInformacio () {
+}
